3328a2123274a7d7497de161d10ebbea053f2af1
somename <someemail>
2017-06-09 14:42:34 +0200

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -8,5 +8,18 @@ public class TestBowling {
 	public void test() {
 		fail("Not yet implemented");
 	}
+	
+	@Test(expected=BowlingException.class){
+	public void test_of_limits throws BowlingException{
+		BowlingGame game= new BowlingGame();
+		game.addFrame(new Frame(0.0));
+		game.addFrame(new Frame(0.0));
+		game.addFrame(new Frame(0.0));
+		game.addFrame(new Frame(0.0));
+		game.addFrame(new Frame(0.0));
+		game.addFrame(new Frame(0.0));
+		game.addFrame(new Frame(0.0));
+		game.addFrame(new Frame(0.0));
+	}
+	}
 
-}
